229d0a0a12c195949577a0277f134e2dc26a16a1
JadenFicklin <jadendficklin@gmail.com>
2022-05-22 17:52:42 -0600

    home page header

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="header">
+        <div className="header-text">OAK AND STONE</div>
+        <div className="header-sub-text">
+          Quality Cabinets and Kitchens by Quality Workers
+        </div>
+      </div>
+      <div className="gray-bar"></div>
+    </>
+  );
+}
+
+export default Home;
